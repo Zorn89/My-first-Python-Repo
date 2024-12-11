72cb53212fbdb356d60c42a12dcc7dc5126894ab
Zorn89 <jan.zorn@tn.techstarter.de>
2024-12-11 13:31:04 +0100

    bitte

--- script.py
+++ script.py
@@ -1,4 +1,14 @@
 first_name = "Jan"
 last_name = "Zorn"
 
-print(first_name + last_name)
+
+zahl1 = 2
+zahl2 = 3
+
+
+print(first_name+" "+last_name)
+
+print("Zahl 1 + Zahl 2:", zahl1 + zahl2) 
+print("Zahl 1 * Zahl 2:", zahl1 * zahl2) 
+print("Zahl 1 / Zahl 2:", zahl1 / zahl2) 
+print("Zahl 2 / Zahl 1:", zahl2 / zahl1) 
