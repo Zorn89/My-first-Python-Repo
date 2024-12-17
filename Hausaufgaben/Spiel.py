9e2d6a6a31f3e57b7f33ce041601ada2536454c9
Zorn89 <jan.zorn@tn.techstarter.de>
2024-12-17 14:35:20 +0100

    merg

--- Hausaufgaben/Spiel.py
+++ Hausaufgaben/Spiel.py
@@ -2,3 +2,15 @@
 
 user_input = input("Wähle (Schere, Stein, Papier)): ")
 result = (user_input)
+if not user_input.isdigit():
+
+
+from random import choice([“Schere”,
+“Stein”, “Papier”]) 
+import random
+
+now = datetime.now()
+print(now)
+print(random.randint(1,10))
+   if not zahl1.isdigit():
+        return "Keine gültige Zahl!"
